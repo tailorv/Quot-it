d966d75f37adaa86021f881bab723c6264fb0293
tailorv <maxwellmuthomijr@gmail.com>
2021-08-24 16:02:25 +0300

    import pipe

--- src/app/publish-date.pipe.ts
+++ src/app/publish-date.pipe.ts
@@ -10,7 +10,7 @@ export class PublishDatePipe implements PipeTransform {
     var dateDifference = Math.abs(value - todayWithNoTime) //returns value in miliseconds
     const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
     var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
-    var dateCounter = dateDifferenceSeconds/secondsInDay;
+    var dateCounter = dateDifferenceSeconds/secondsInDa;
 
     if (dateCounter >= 1 && value > todayWithNoTime){
       return dateCounter;
